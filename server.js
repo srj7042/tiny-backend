bfcba96c2977790d76f8193986183285656f5361
Suraj Jaiswal <surajjaiswal4222@gmail.com>
2026-07-10 23:48:24 +0530

    Initial backend assignment

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    name: "Your Name",
+    course: "Backend Assignment"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
